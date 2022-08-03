259e2c826230c1426130b155b8ce391abfcfe5cd
yujian.sun <yujian.sun@dmall.com>
2022-08-03 23:56:13 +0800

    feat: iOS白屏的问题

---
+++ ios/SplashScreen.h
@@ -0,0 +1,16 @@
+//
+//  SplashScreen.h
+//  MyApp
+//
+//  Created by 孙玉建 on 2022/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SplashScreen : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/SplashScreen.m
@@ -0,0 +1,12 @@
+//
+//  SplashScreen.m
+//  MyApp
+//
+//  Created by 孙玉建 on 2022/8/3.
+//
+
+#import "SplashScreen.h"
+
+@implementation SplashScreen
+
+@end
